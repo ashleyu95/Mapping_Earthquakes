0667e03dcd4d2d6d48eaa64697fb091fa7c9dba3
yyu52 <yyu52@fordham.edu>
2021-03-25 14:09:24 +1300

    back to main

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -12,7 +12,7 @@ cityData.forEach(function(city) {
     console.log(city)
     L.circleMarker(city.location, {
     radius: city.population/100000,
-    color: "yellow",
+    color: "orange",
     fillcolor: '#ffffa1'
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
